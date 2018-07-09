d3344e2b15770e43f79adb6ee8971f13ef7dff8d
Raniel <r.a_tw@hotmail.com>
2018-07-09 16:40:17 -0300

    como saber quando o modelo mudou?(alterando a view automaticamente quando adiciona-se ou apaga-se dados da tabela), arrow function possui contexto léxico, function possui contexto dinamico

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,13 @@ class NegociacaoController {
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
-		this._listaNegociacoes = new ListaNegociacoes();
+		this._listaNegociacoes = new ListaNegociacoes(model =>	//o this na arrow function é léxico, então ele continua a se referir ao NegociacaoController
+											this._negociacoesView.update(this._listaNegociacoes));
+
+		//aqui é como teria que fazer com uma função normal ao invés de arrow function
+		//this._listaNegociacoes = new ListaNegociacoes(this, function (model) { //cria uma função junto que irá atualizar a View(tabela)
+		//	this._negociacoesView.update(this._listaNegociacoes);		//a function tem this dinamico, então este this sem utilizar o Reflect
+		//});																//na ListaNegociacoes se refere a ListaNegociacoes
 
 		this._negociacoesView = new NegociacoesView($("#negociacoesView"));
 		this._negociacoesView.update(this._listaNegociacoes);
@@ -23,13 +29,21 @@ class NegociacaoController {
 		this._listaNegociacoes.adiciona(this._criaNegociacao());			
 		this._negociacoesView.update(this._listaNegociacoes);
 
-		this._mensagem.texto = "Negociacao adicionada com sucesso";	
+		this._mensagem.texto = "Negociação adicionada com sucesso";	
 		this._mensagemView.update(this._mensagem);
 
 		this._limpaFormulario();
 
 		//console.log(this._listaNegociacoes.negociacoes);
 		
+	}
+
+	apaga(){
+		this._listaNegociacoes.esvazia();
+		this._negociacoesView.update(this._listaNegociacoes);
+
+		this._mensagem.texto = "Negociações apagadas com sucesso";	
+		this._mensagemView.update(this._mensagem);
 	}
 
 	_limpaFormulario(){
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,29 @@
 class ListaNegociacoes{
 
-	constructor(){
+	constructor(armadilha){ //recebendo o contexto do NegociacaoController e uma função
 		this._negociacoes = [];
+		this._armadilha = armadilha;
 	}
 
 	adiciona(negociacao){
 		this._negociacoes.push(negociacao);
+		this._armadilha(this);
+		//Reflect.apply(this._armadilha, this._contexto, [this]); //usando reflection para trocar o contexto da função presente no NegociacaoController, que seria o da ListaNegociacao mas agora passa a ser o da NegociacaoController mesmo.
+					//ordem construtor Reflect.Apply(função, contexto da função, parametros no construtor da função):
+					//função que quer chamar, 
+					//o contexto em que a função está presente(neste caso a função esta no NegociacaoController, por isso foi passado seu contexto para ca pelo construtor (constructor(contexto, armadilha) tinha esse construtor antes usar arrow function)),
+					//os parametros do construtor da função dentro de um array, nesse caso ela recebe o model(que é a ListaNegociacoes)
+	}
+
+	esvazia(){
+		this._negociacoes = [];
+		this._armadilha(this);
+		//Reflect.apply(this._armadilha, this._contexto, [this]);
 	}
 
 	get negociacoes(){
 		return [].concat(this._negociacoes); //concatenando um array vazion com a lista da classe para evitar
 											 //que outras classes possam ter acesso a lista interna
+		
 	}
 }
