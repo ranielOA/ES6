6de9697355ca0d6d1e30f319e112ac51286d2f59
Raniel <r.a_tw@hotmail.com>
2018-07-11 22:52:42 -0300

    fazendo melhorias na criação do Proxy

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -8,21 +8,22 @@ class ProxyFactory{
                 if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])){
                 	return function(){
 	                    console.log(`a propriedade "${prop}" foi interceptada`);//aqui pode ser executado algun código antes da função do objeto ser chamada(por exemplo antes de adicionar ou esvaziar a tabela)
-	                    Reflect.apply(target[prop], target, arguments);// aqui aplica a função do objeto(pode ser a ListaNegociacoes.adiciona() por exemplo
-	                    return acao(target); //aqui ele chama a função que ta lá no construtor quando faz o ProxyFactory.create()
+	                    let retorno = Reflect.apply(target[prop], target, arguments);// aqui aplica a função do objeto(pode ser a ListaNegociacoes.adiciona() por exemplo
+	                    acao(target); //aqui ele chama a função que ta lá no construtor quando faz o ProxyFactory.create()
+	                	return retorno;
 	                }
                 }
 
-                return Reflect.get(target, prop, receiver);
+                return Reflect.get(target, prop, receiver); //deixa o get seguir seu curso
             },
 
             set(target, prop, value, receiver){//o target eh o objeto, prop suas propriedades, value o novo valor que a propriedade receberá e receiver o proxy
 
+            	let retorno = Reflect.set(target, prop, value, receiver);// faz o set atribuindo o valor novo a variável
             	if(props.includes(prop)){
-            		target[prop] = value;//atribuindo o novo valor a propriedade(exemplo: Mensagem[texto] = novo texto)
-            		acao(target);// então chama a função que foi colocada no construtor quando faz o ProxyFactory.create()
+            		acao(target);//chama a função que foi colocada no construtor quando faz o ProxyFactory.create()
             	}
-            	return Reflect.set(target, prop, value, receiver);
+            	return retorno;
             }
         });
 	}
